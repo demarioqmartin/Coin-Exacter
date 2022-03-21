5649a9c00bf47e514308b28a8ff302f4c7c21bd9
Demario Martin <demario.q.martin@outlook.com>
2022-03-21 12:46:08 -0400

    Add project files.

---
+++ Coin Exacter/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinExacter
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Money Maker!");
+
+            Console.WriteLine("Enter the amount to convert: ");
+            string amount = Console.ReadLine();
+            double userAmount = Convert.ToDouble(amount);
+
+
+            int Quarter = 25;
+            int Dime = 10;
+            int Nickel = 5;
+
+            double quart = Math.Floor(userAmount / Quarter);
+            double remainder = userAmount % Quarter;
+
+            double dim = Math.Floor(remainder / Dime);
+            double remainder2 = remainder % Dime;
+
+            double nick = Math.Floor(remainder2 / Nickel);
+            double remainder3 = remainder2 % Nickel;
+
+            Console.WriteLine($"{userAmount} cents is equal to: \nquarter(s): {quart} \ndime(s): {dim} \nnickel(s):{nick} \nPennies {remainder3}");
+
+        }
+    }
+}
